482f2b313c13a86a61f3a80de23f55fb5e2d4871
wgarcia <walter.garcia@upf.edu>
2020-10-08 13:07:13 +0200

    Correccion cars93

--- Analisi_de_Dades/Dispersio/initLesson.R
+++ Analisi_de_Dades/Dispersio/initLesson.R
@@ -1,2 +1,2 @@
-ssign("cars", openintro::cars93, envir=globalenv())
+assign("cars", openintro::cars93, envir=globalenv())
 assign("mpg.midsize", cars[cars93$type=="midsize","mpg_city"], envir=globalenv())
